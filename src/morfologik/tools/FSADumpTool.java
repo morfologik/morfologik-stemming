9da8866f02bd18e3bcf7f680d376f531219ce78d
Dawid Weiss <dawid.weiss@gmail.com>
2009-03-26 10:28:43 +0000

    Dumping additional information about metadata.

--- src/morfologik/tools/FSADumpTool.java
+++ src/morfologik/tools/FSADumpTool.java
@@ -2,13 +2,13 @@ package morfologik.tools;
 
 import java.io.*;
 import java.nio.ByteBuffer;
-import java.nio.charset.*;
-import java.text.MessageFormat;
+import java.nio.charset.Charset;
+import java.util.Locale;
+import java.util.Map;
 
 import morfologik.fsa.FSA;
 import morfologik.fsa.FSAHelpers;
 import morfologik.stemming.Dictionary;
-import morfologik.util.Arrays;
 import morfologik.util.FileUtils;
 
 import org.apache.commons.cli.CommandLine;
@@ -20,51 +20,34 @@ import org.apache.commons.cli.Options;
  */
 public final class FSADumpTool extends Tool {
     /**
-     * @see #word
-     */
-    private static final int MAX_BUFFER_SIZE = 5000;
-
-    /**
-     * @see #word
-     */
-    private static final int BUFFER_INCREMENT = 1000;
-
-    /**
      * Writer used to print messages and dictionary dump.
      */
-    private PrintStream writer;
+    private OutputStream writer;
 
     /**
-     * Encoding of the symbols inside the FSA.
+     * Print raw data only, no headers.
      */
-    private String encoding;
+    private boolean dataOnly;
 
     /**
-     * The array where all labels on a path from start to final node are
-     * collected. The buffer automatically expands up to {@link MAX_BUFFER_SIZE}
-     * when an exception is thrown.
-     */
-    private static byte[] word = new byte[BUFFER_INCREMENT];
-
-    /**
-     * 
+     * Command line entry point after parsing arguments.
      */
     protected void go(CommandLine line) throws Exception {
-	final File dictionaryFile = (File) line
-		.getParsedOptionValue(SharedOptions.fsaDictionaryFileOption
-			.getOpt());
+	final File dictionaryFile = (File) line.getParsedOptionValue(
+		SharedOptions.fsaDictionaryFileOption.getOpt());
+
+	dataOnly = line.hasOption(
+		SharedOptions.dataOnly.getOpt());
+
 	FileUtils.assertExists(dictionaryFile, true, false);
 
-	final boolean useAPI = line.hasOption(SharedOptions.useApiOption
-		.getOpt());
-
-	dump(dictionaryFile, useAPI);
+	dump(dictionaryFile, dataOnly);
     }
 
     /**
      * Dumps the content of a dictionary to a file.
      */
-    private void dump(File dictionaryFile, boolean useAPI)
+    private void dump(File dictionaryFile, boolean dataOnly)
 	    throws UnsupportedEncodingException, IOException {
 	final long start = System.currentTimeMillis();
 
@@ -72,134 +55,108 @@ public final class FSADumpTool extends Tool {
 	final FSA fsa;
 
 	if (!dictionaryFile.canRead()) {
-	    writer.println("Dictionary file does not exist: "
+	    System.err.println("Dictionary file does not exist: " 
 		    + dictionaryFile.getAbsolutePath());
 	    return;
 	}
 
-	this.writer = System.out;
+	this.writer = new BufferedOutputStream(System.out, 1024 * 32);
 
-	if (hasFeatures(dictionaryFile)) {
+	if (hasMetadata(dictionaryFile)) {
 	    dictionary = Dictionary.read(dictionaryFile);
 	    fsa = dictionary.fsa;
-	    this.encoding = dictionary.metadata.encoding;
 
+	    final String encoding = dictionary.metadata.encoding;
 	    if (!Charset.isSupported(encoding)) {
-		writer
-			.println("Dictionary's charset is not supported on this JVM: "
-				+ encoding);
+		printExtra("Dictionary's charset is not supported "
+			+ "on this JVM: " + encoding);
 		return;
 	    }
 	} else {
-	    encoding = null;
 	    dictionary = null;
 	    fsa = FSA.getInstance(dictionaryFile, "iso-8859-1");
-	    writer
-		    .println("Warning: FSA automaton without metadata file. Raw bytes emitted.");
+	    printExtra("Warning: FSA automaton without metadata file.");
 	}
 
-	writer.println("FSA properties");
-	writer.println("--------------------");
-	writer.println("FSA file version    : " + fsa.getVersion());
-	writer.println("Compiled with flags : "
+	printExtra("FSA properties");
+	printExtra("--------------------");
+	printExtra("FSA file version    : " + fsa.getVersion());
+	printExtra("Compiled with flags : "
 		+ FSAHelpers.flagsToString(fsa.getFlags()));
-	writer.println("Number of arcs      : " + fsa.getNumberOfArcs());
-	writer.println("Number of nodes     : " + fsa.getNumberOfNodes());
-	writer.println("Annotation separator: " + fsa.getAnnotationSeparator());
-	writer.println("Filler character    : " + fsa.getFillerCharacter());
-	writer.println("");
+	printExtra("Number of arcs      : " + fsa.getNumberOfArcs());
+	printExtra("Number of nodes     : " + fsa.getNumberOfNodes());
+	printExtra("Annotation separator: " + fsa.getAnnotationSeparator());
+	printExtra("Filler character    : " + fsa.getFillerCharacter());
+	printExtra("");
 
 	if (dictionary != null) {
-	    final CharsetDecoder decoder = Charset.forName(encoding)
-		    .newDecoder();
-	    decoder.onUnmappableCharacter(CodingErrorAction.REPORT);
-	    decoder.onMalformedInput(CodingErrorAction.REPORT);
-	    writer.println("Dictionary metadata");
-	    writer.println("--------------------");
-	    writer.println("Encoding            : "
-		    + dictionary.metadata.encoding);
-	    writer
-		    .println("Separator           : "
-			    + decoder
-				    .decode(ByteBuffer
-					    .wrap(new byte[] { dictionary.metadata.separator })));
-	    writer.println("Uses prefixes       : "
-		    + dictionary.metadata.usesPrefixes);
-	    writer.println("Uses infixes        : "
-		    + dictionary.metadata.usesInfixes);
-	    writer.println("");
-	}
+	    printExtra("Dictionary metadata");
+	    printExtra("--------------------");
+	    printExtra("Encoding            : " + dictionary.metadata.encoding);
+	    printExtra("Separator byte      : 0x" 
+		    + Integer.toHexString(dictionary.metadata.separator)
+		    + " ('" + decodeSeparator(dictionary) + "')");
+	    printExtra("Uses prefixes       : " + dictionary.metadata.usesPrefixes);
+	    printExtra("Uses infixes        : " + dictionary.metadata.usesInfixes);
+	    printExtra("");
 
-	writer.println("FSA data");
-	writer.println("--------------------");
+	    printExtra("Dictionary metadata (all keys)");
+	    printExtra("---------------------------------");
 
-	if (useAPI) {
-	    if (encoding != null) {
-		for (ByteBuffer bb : fsa) {
-		    writer.println(new String(bb.array(), 0, bb.remaining(),
-			    encoding));
-		}
-	    } else {
-		for (ByteBuffer bb : fsa) {
-		    writer.write(bb.array(), 0, bb.remaining());
-		    writer.println();
-		}
+	    for (Map.Entry<String, String> e : dictionary.metadata.metadata.entrySet())
+	    {
+		printExtra(String.format("%-27s : %s", e.getKey(), e.getValue()));
 	    }
-	} else {
-	    dumpNode(fsa, fsa.getRootNode(), 0);
+	    printExtra("");
 	}
 
-	writer.println("--------------------");
+	printExtra("FSA data (raw bytes in the encoding above)");
+	printExtra("------------------------------------------");
 
-	final long millis = System.currentTimeMillis() - start;
-	writer.println(MessageFormat.format(
-		"Dictionary dumped in {0,number,#.###} seconds.",
-		new Object[] { new Double(millis / 1000.0) }));
+	int sequences = 0;
+	for (ByteBuffer bb : fsa) {
+	    writer.write(bb.array(), 0, bb.remaining());
+	    writer.write(0x0a);
+	    sequences++;
+	}
+
+	printExtra("--------------------");
+
+	final long millis = Math.max(1, System.currentTimeMillis() - start);
+	printExtra(String.format(Locale.ENGLISH,
+		"Dictionary dumped in %.3f second(s), %d sequences (%d sequences/sec.).",
+		millis / 1000.0,
+		sequences,
+		(int) (sequences / (millis / 1000.0))));
+
+	writer.flush();
     }
 
-    /**
-     * Called recursively traverses the automaton.
+    /*
+     * 
      */
-    private void dumpNode(FSA fsa, int node, final int depth)
-	    throws UnsupportedEncodingException {
-	int arc = fsa.getFirstArc(node);
-	do {
-	    if (depth >= word.length) {
-		if (word.length + BUFFER_INCREMENT > MAX_BUFFER_SIZE) {
-		    throw new RuntimeException("Error: Buffer limit of "
-			    + MAX_BUFFER_SIZE
-			    + " bytes exceeded. A loop in the automaton maybe?");
-		}
+    private void printExtra(String msg) throws IOException {
+	if (dataOnly) return;
+	writer.write(msg.getBytes());
+	writer.write(0x0a);
+    }
 
-		word = Arrays.copyOf(word, word.length + BUFFER_INCREMENT);
-
-		// Redo the operation.
-		word[depth] = fsa.getArcLabel(arc);
-	    }
-
-	    word[depth] = fsa.getArcLabel(arc);
-
-	    if (fsa.isArcFinal(arc)) {
-		if (encoding != null) {
-		    writer.println(new String(word, 0, depth + 1, encoding));
-		} else {
-		    writer.write(word, 0, depth + 1);
-		    writer.println();
-		}
-	    }
-
-	    if (fsa.isArcTerminal(arc)) {
-		dumpNode(fsa, fsa.getEndNode(arc), depth + 1);
-	    }
-
-	    arc = fsa.getNextArc(node, arc);
-	} while (arc != 0);
+    /*
+     * 
+     */
+    private String decodeSeparator(Dictionary dictionary) {
+	try {
+	    return new String(new byte[] { dictionary.metadata.separator },
+		    dictionary.metadata.encoding);
+	} catch (UnsupportedEncodingException e) {
+	    return "<unsupported encoding: " + dictionary.metadata.encoding + ">"; 
+	}
     }
 
     /**
      * Check if there is a metadata file for the given FSA automaton.
      */
-    private static boolean hasFeatures(File fsaFile) {
+    private static boolean hasMetadata(File fsaFile) {
 	final File featuresFile = new File(fsaFile.getParent(), Dictionary
 		.getExpectedFeaturesName(fsaFile.getName()));
 
@@ -211,7 +168,7 @@ public final class FSADumpTool extends Tool {
      */
     protected void initializeOptions(Options options) {
 	options.addOption(SharedOptions.fsaDictionaryFileOption);
-	options.addOption(SharedOptions.useApiOption);
+	options.addOption(SharedOptions.dataOnly);
     }
 
     /**
