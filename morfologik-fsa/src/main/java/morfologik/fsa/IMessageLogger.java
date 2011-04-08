1039d6dcb192c19197e0562171fa80cbcb67d8aa
Dawid Weiss <dawid.weiss@gmail.com>
2011-04-08 21:48:29 +0000

    project split, initial.

---
+++ morfologik-fsa/src/main/java/morfologik/fsa/IMessageLogger.java
@@ -0,0 +1,25 @@
+package morfologik.fsa;
+
+public interface IMessageLogger {
+
+    /**
+     * Log progress to the console.
+     */
+    public void log(String msg);
+
+    /**
+     * Log message header and save current time.
+     */
+    public void startPart(String header);
+
+    /**
+     * 
+     */
+    public void endPart();
+
+    /**
+     * Log a two-part message.
+     */
+    public void log(String header, Object v);
+
+}
