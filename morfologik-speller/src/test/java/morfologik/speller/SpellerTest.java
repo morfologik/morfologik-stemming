3072e2400f2fa926ad4130b34c691e1424648902
Dawid Weiss <dawid.weiss@gmail.com>
2012-06-07 21:44:13 +0000

    Fixing resources.

--- morfologik-speller/src/test/java/morfologik/speller/SpellerTest.java
+++ morfologik-speller/src/test/java/morfologik/speller/SpellerTest.java
@@ -1,6 +1,6 @@
 package morfologik.speller;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
 import java.net.URL;
@@ -8,22 +8,19 @@ import java.net.URL;
 import morflogik.speller.Speller;
 import morfologik.stemming.Dictionary;
 
-import org.junit.Ignore;
 import org.junit.Test;
 
-@Ignore("Fails with an assertion!")
 public class SpellerTest {
-
 	@Test
 	public void testRunonWords() throws IOException {
-		final URL url = this.getClass().getResource("slownik.dict");		
+        final URL url = getClass().getResource("slownik.dict");		
 		final Speller spell = new Speller(Dictionary.read(url));
 		assertTrue(spell.replaceRunOnWords("abaka").isEmpty());
 		assertTrue(spell.replaceRunOnWords("abakaabace").
 				contains("abaka abace"));
-		
-		//test on an morphological dictionary - should work as well
-		final URL url1 = this.getClass().getResource("test-infix.dict");		
+
+		// Test on an morphological dictionary - should work as well
+		final URL url1 = getClass().getResource("test-infix.dict");		
 		final Speller spell1 = new Speller(Dictionary.read(url1));
 		assertTrue(spell1.replaceRunOnWords("Rzekunia").isEmpty());
 		assertTrue(spell1.replaceRunOnWords("RzekuniaRzeczypospolitej").
@@ -32,11 +29,11 @@ public class SpellerTest {
 	
 	@Test
 	public void testFindReplacements() throws IOException {
-		final URL url = this.getClass().getResource("slownik.dict");		
+		final URL url = getClass().getResource("slownik.dict");		
 		final Speller spell = new Speller(Dictionary.read(url), 2);
 		assertTrue(spell.findReplacements("abka").contains("abak"));
 		
-		final URL url1 = this.getClass().getResource("test-infix.dict");		
+		final URL url1 = getClass().getResource("test-infix.dict");		
 		final Speller spell1 = new Speller(Dictionary.read(url1));
 		assertTrue(spell1.findReplacements("Rezkunia").contains("Rzekunia"));
 	}	
