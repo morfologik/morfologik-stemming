fcf12566584f682c73fa5a8cd0975b5f2bea40d7
Dawid Weiss <dawid.weiss@carrotsearch.com>
2013-10-18 23:27:08 +0200

    More work on prefix encoding.

--- morfologik-tools/src/test/java/morfologik/tools/MorphEncoder2.java
+++ morfologik-tools/src/test/java/morfologik/tools/MorphEncoder2.java
@@ -10,34 +10,87 @@ public final class MorphEncoder2 {
      */
     private static final int REMOVE_EVERYTHING = 255;
 
-    public static ByteArrayList encodeSuffix(ByteArrayList src, ByteArrayList dst, ByteArrayList encoded)
-    {
-        int sharedPrefix = sharedPrefixLength(src, dst);
-        int truncateBytes = src.size() - sharedPrefix;
-        if (truncateBytes >= REMOVE_EVERYTHING) {
-            truncateBytes = REMOVE_EVERYTHING;
-            sharedPrefix = 0;
-        }
-
-        final byte suffixTrimCode = (byte) (truncateBytes + 'A');
-        encoded.add(suffixTrimCode);
-        encoded.add(dst.buffer, sharedPrefix, dst.size() - sharedPrefix);
-
-        return encoded;
+    public static interface IEncoder {
+        public ByteArrayList encode(ByteArrayList src, ByteArrayList derived, ByteArrayList encodedBuffer);
+        public ByteArrayList decode(ByteArrayList src, ByteArrayList encoded, ByteArrayList derivedBuffer);
     }
 
-    public static ByteArrayList decodeSuffix(ByteArrayList src, ByteArrayList encoded, ByteArrayList dst)
-    {
-        int suffixTrimCode = encoded.get(0);
-        int truncateBytes = (suffixTrimCode - 'A') & 0xFF;
-        if (truncateBytes == REMOVE_EVERYTHING) {
-            truncateBytes = src.size();
+    /**
+     * TODO: add javadoc on encoding format.
+     */
+    public static class TrimSuffixEncoder implements IEncoder {
+        public ByteArrayList encode(ByteArrayList src, ByteArrayList dst, ByteArrayList encoded) {
+            int sharedPrefix = sharedPrefixLength(src, dst);
+            int truncateBytes = src.size() - sharedPrefix;
+            if (truncateBytes >= REMOVE_EVERYTHING) {
+                truncateBytes = REMOVE_EVERYTHING;
+                sharedPrefix = 0;
+            }
+
+            final byte suffixTrimCode = (byte) (truncateBytes + 'A');
+            encoded.add(suffixTrimCode);
+            encoded.add(dst.buffer, sharedPrefix, dst.size() - sharedPrefix);
+
+            return encoded;
         }
 
-        dst.add(src.buffer, 0, src.size() - truncateBytes);
-        dst.add(encoded.buffer, 1, encoded.size() - 1);
+        public ByteArrayList decode(ByteArrayList src, ByteArrayList encoded, ByteArrayList dst) {
+            int suffixTrimCode = encoded.get(0);
+            int truncateBytes = (suffixTrimCode - 'A') & 0xFF;
+            if (truncateBytes == REMOVE_EVERYTHING) {
+                truncateBytes = src.size();
+            }
 
-        return dst;
+            dst.add(src.buffer, 0, src.size() - truncateBytes);
+            dst.add(encoded.buffer, 1, encoded.size() - 1);
+
+            return dst;
+        }
+        
+        @Override
+        public String toString() {
+            return "TrimSuffixEncoder";
+        }
+    }
+
+    /**
+     * TODO: add javadoc on encoding format.
+     */
+    public static class TrimPrefixAndSuffixEncoder implements IEncoder {
+        public ByteArrayList encode(ByteArrayList src, ByteArrayList dst, ByteArrayList encoded) {
+            // Search for the maximum matching subsequence that is encodable. 
+            int maxSubsequenceLength = 0;
+            int maxSubsequenceIndex = 0;
+            for (int i = 0; i < src.size(); i++) {
+                int sharedPrefix = sharedPrefixLength(src, i, dst, 0);
+                if (sharedPrefix > maxSubsequenceLength
+                        && i < REMOVE_EVERYTHING
+                        && (src.size() - (i + sharedPrefix)) < REMOVE_EVERYTHING) {
+                    maxSubsequenceLength = sharedPrefix;
+                    maxSubsequenceIndex = i;
+                }
+            }
+
+            int truncatePrefixBytes = maxSubsequenceIndex;
+            int truncateSuffixBytes = (src.size() - (maxSubsequenceIndex + maxSubsequenceLength));
+
+            if (truncatePrefixBytes >= REMOVE_EVERYTHING ||
+                truncateSuffixBytes >= REMOVE_EVERYTHING) {
+                maxSubsequenceIndex = maxSubsequenceLength = 0;
+                truncatePrefixBytes = truncateSuffixBytes = REMOVE_EVERYTHING;
+            }
+
+            encoded.add((byte) ((truncatePrefixBytes + 'A') & 0xFF));
+            encoded.add((byte) ((truncateSuffixBytes + 'A') & 0xFF));
+            encoded.add(dst.buffer, maxSubsequenceLength, dst.size() - maxSubsequenceLength);
+
+            return encoded;
+        }
+
+        public ByteArrayList decode(ByteArrayList src, ByteArrayList encoded, ByteArrayList dst) {
+            // TODO: write me.
+            return dst;
+        }
     }
 
     /**
@@ -47,6 +100,20 @@ public final class MorphEncoder2 {
         final int max = Math.min(a.size(), b.size());
         int i = 0;
         while (i < max && a.get(i) == b.get(i)) {
+            i++;
+        }
+        return i;
+    }
+    
+    /**
+     * Compute the length of the shared prefix between two byte sequences.
+     */
+    private static int sharedPrefixLength(ByteArrayList a, int aStart, ByteArrayList b, int bStart) {
+
+        int i = 0;
+        while (aStart < a.size() && 
+               bStart < b.size() &&
+               a.get(aStart++) == b.get(bStart++)) {
             i++;
         }
         return i;
--- morfologik-tools/src/test/java/morfologik/tools/MorphEncoder2Test.java
+++ morfologik-tools/src/test/java/morfologik/tools/MorphEncoder2Test.java
@@ -2,14 +2,30 @@ package morfologik.tools;
 
 import static com.google.common.base.Charsets.UTF_8;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.junit.Test;
 
 import com.carrotsearch.hppc.ByteArrayList;
 import com.carrotsearch.randomizedtesting.RandomizedTest;
-
-import static morfologik.tools.MorphEncoder2.*;
+import com.carrotsearch.randomizedtesting.annotations.Name;
+import com.carrotsearch.randomizedtesting.annotations.ParametersFactory;
 
 public class MorphEncoder2Test extends RandomizedTest {
+    
+    private final MorphEncoder2.IEncoder coder;
+
+    public MorphEncoder2Test(@Name("coder") MorphEncoder2.IEncoder coder)
+    {
+        this.coder = coder;
+    }
+
+    @ParametersFactory
+    public static List<Object[]> testFactory() {
+        return Arrays.asList($$($(new MorphEncoder2.TrimSuffixEncoder())));
+    }
+
     @Test
     public void testEncodeSuffixOnRandomSequences() {
         for (int i = 0; i < 10000; i++) {
@@ -35,23 +51,21 @@ public class MorphEncoder2Test extends RandomizedTest {
         ByteArrayList encoded = ByteArrayList.newInstance();
         ByteArrayList decoded = ByteArrayList.newInstance();
 
-        encodeSuffix(src, dst, encoded);
-        decodeSuffix(src, encoded, decoded);
+        coder.encode(src, dst, encoded);
+        coder.decode(src, encoded, decoded);
 
         assertEquals(dst, decoded);
     }
-    
 
     public static void main(String [] args) {
         //ByteBuffer src = ByteBuffer.wrap("x123foo45abc".getBytes(UTF_8));
         //ByteBuffer dst = ByteBuffer.wrap("12345def".getBytes(UTF_8));
+        ByteArrayList src = ByteArrayList.from("0123123456789".getBytes(UTF_8));
+        ByteArrayList dst = ByteArrayList.from("123456789X".getBytes(UTF_8));
 
-        ByteArrayList src = ByteArrayList.from("abcd".getBytes(UTF_8));
-        ByteArrayList dst = ByteArrayList.from("abcx".getBytes(UTF_8));
-
-        ByteArrayList out = encodeSuffix(src, dst, ByteArrayList.newInstance());
+        MorphEncoder2.IEncoder coder = new MorphEncoder2.TrimPrefixAndSuffixEncoder();
+        ByteArrayList out = coder.encode(src, dst, ByteArrayList.newInstance());
         System.out.println(new String(out.toArray()));
-        
-        System.out.println(new String(decodeSuffix(src, out, ByteArrayList.newInstance()).toArray()));
+        System.out.println(new String(coder.decode(src, out, ByteArrayList.newInstance()).toArray()));
     }    
 }
