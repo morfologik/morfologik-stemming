dcc9cb01693766317f18ef720db03aca38a48b2d
Dawid Weiss <dawid.weiss@carrotsearch.com>
2014-08-11 14:23:10 +0200

    The output makes sense. Not a bug.:

---
+++ morfologik-polish/src/test/java/morfologik/stemming/Gh27Test.java
@@ -0,0 +1,29 @@
+package morfologik.stemming;
+
+import java.io.IOException;
+import java.util.Locale;
+
+import org.junit.Test;
+
+/*
+ * 
+ */
+public class Gh27Test {
+	/* */
+	@Test
+	public void gh27() throws IOException {
+		PolishStemmer stemmer = new PolishStemmer();
+
+		String in = "Nie zabrakło oczywiście wpadek. Największym zaskoczeniem okazał się dla nas strój Katarzyny Zielińskiej, której ewidentnie o coś chodziło, ale wciąż nie wiemy o co.";
+		for (String t : in.toLowerCase(new Locale("pl")).split("[\\s\\.\\,]+")) {
+		  System.out.println("> '" + t + "'");
+		  for (WordData wd : stemmer.lookup(t)) {
+		    System.out.print(
+		        "  - " + 
+		        (wd.getStem() == null ? "<null>" : wd.getStem()) + ", " + 
+		        wd.getTag());
+		  }
+		  System.out.println();
+		}
+	}
+}
